0229dfdab541f9be2b11eb994abf1e1f2c502fce
Amarjeet261 <amarjeetrajput326@gmail.com>
2025-01-17 00:51:18 +0530

    some changes to html and css file

---
+++ potions/show.js
@@ -0,0 +1,22 @@
+const url = "https://api.potterdb.com/v1/potions?page[number]=1&page[size]=100";
+const url2 = "https://api.potterdb.com/v1/potions?page[number]=2&page[size]=68";
+
+
+
+
+
+const potionimg = document.querySelectorAll(".potion-img > img");
+const potionName = document.querySelectorAll(".potion-name");
+const potionDiff = document.querySelectorAll(".difficulty");
+const potioneffect = document.querySelectorAll(".effect");
+const potionChar = document.querySelectorAll(".character");
+
+
+async function getData(url) {
+    
+}
+
+
+
+
+
