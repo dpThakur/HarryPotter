a9d73878c0b1440f363d4efea20544f883a38aca
Amarjeet261 <amarjeetrajput326@gmail.com>
2025-01-23 22:38:20 +0530

    added somepart of viewpotion

--- potions/show.js
+++ potions/show.js
@@ -5,18 +5,4 @@ const url2 = "https://api.potterdb.com/v1/potions?page[number]=2&page[size]=68";
 
 
 
-const potionimg = document.querySelectorAll(".potion-img > img");
-const potionName = document.querySelectorAll(".potion-name");
-const potionDiff = document.querySelectorAll(".difficulty");
-const potioneffect = document.querySelectorAll(".effect");
-const potionChar = document.querySelectorAll(".character");
-
-
-async function getData(url) {
-    
-}
-
-
-
-
 
