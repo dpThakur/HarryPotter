d9e7e3e8bfa37c8c71113be1a9e01205bab06d5a
suraj-web <shakyasuraj595@gamil.com>
2025-01-24 21:10:44 +0530

    added

--- spells/spells.js
+++ spells/spells.js
@@ -1,3 +1,56 @@
+// create data 
+
+ 
+const header = document.querySelector("header");
+const mainplay = document.createElement("div");
+mainplay.className = "display display1";
+header.prepend(mainplay);
+
+for (let i = 1; i <= 40; i++) {
+
+    const displaycreate = document.createElement("div");
+    displaycreate.className = "cards";
+
+    mainplay.append(displaycreate);
+    // cards create 
+
+    const createcard = document.createElement("div");
+    createcard.className = "card";
+    // card create 
+
+    const createimg = document.createElement("img");
+    createcard.append(createimg);
+
+    // img create 
+    const disul = document.createElement("ul");
+    // ul create 
+    const disli = document.createElement("li");
+    disli.className = "name";
+    // li1 create 
+
+    const disli1 = document.createElement("li");
+    disli1.className = "charm";
+    // li2 create 
+
+    const disli2 = document.createElement("li");
+    disli2.className = "topi";
+    // li3 create 
+
+    const anker1 = document.createElement("a");
+    anker1.className = "anker";
+    // anker create 
+
+    anker1.href = "viewspells.html";
+    anker1.innerHTML = "View spells";
+    disul.append(disli, disli1, disli2, anker1);
+    displaycreate.append(createcard, disul);
+
+};
+
+
+// end 
+
+
 // apis 
 
 const url1 = "https://api.potterdb.com/v1/spells?page[number]=1&page[size]=100";
@@ -5,9 +58,9 @@ const url2 = "https://api.potterdb.com/v1/spells?page[number]=2&page[size]=100";
 const url3 = "https://api.potterdb.com/v1/spells?page[number]=3&page[size]=100";
 const url4 = "https://api.potterdb.com/v1/spells?page[number]=3&page[size]=16";
 
-// apis end 
+// apis end
 
-// card Select 
+// card Select
 
 const cardsimage = document.querySelectorAll(".card > img");
 const cardsname = document.querySelectorAll(".name");
@@ -15,9 +68,9 @@ const cardscharm = document.querySelectorAll(".charm");
 const cardstpoi = document.querySelectorAll(".topi");
 const hloworld = document.querySelector(".hlo-world >li");
 const ankerlink = document.querySelectorAll(".anker")
-// end 
+// end
 
-// get data 
+// get data
 async function getData(url) {
     let jsonData = await fetch(url);
     let jsObject = await jsonData.json();
@@ -35,8 +88,10 @@ async function getData(url) {
 
     let datafull = [data1, data2, data3, data4].flat()
     datafull.forEach((datas, idx) => {
+
         if (datas.attributes.image) {
             cardsimage[idx].src = datas.attributes.image;
+
         }
         else {
             cardsimage[idx].src = "https://potterdb.com/images/missing_spell.svg";
@@ -46,25 +101,24 @@ async function getData(url) {
         cardscharm[idx].innerHTML = datas.attributes.category;
         cardstpoi[idx].innerHTML = datas.attributes.incantation;
         hloworld.innerHTML = datafull.length;
-        ankerlink.href = ankerlink[idx].href + `?id=${datas.id}`;
-        
-        
-        
-        
+        ankerlink.href = ankerlink.href + `?id=${datas.id}`;
+
+
+
 
     });
 
 }
 )()
 
-// end 
+// end
 
-// loadmore 
+// loadmore
 
 
 let displayIndex = 1;
 
-const display = document.querySelectorAll(".display");
+
 const loadmore = document.querySelector(".load");
 loadmore.addEventListener("click", myFunction);
 function myFunction() {
@@ -83,9 +137,9 @@ function myFunction() {
 
 };
 
-// end 
+// end
 
-// addEventListener 
+// addEventListener
 
 const liicon = document.querySelector(".licontainer");
 const svgicon = document.querySelector(".svg");
@@ -104,16 +158,3 @@ function cutmark() {
 };
 
 // end 
-
-
-
-
-
-
-
-
-
-
-
-
-
